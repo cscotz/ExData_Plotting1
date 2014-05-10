bfaebee4bbddcabe74ab37f649d096bca7ef0361
Chris Scott <cscott83@gmail.com>
2014-05-10 13:43:34 -0400

    Update plot function to use the with() construct Modify read function to avoid factors and specify na strings

--- plot2.R
+++ plot2.R
@@ -1,14 +1,17 @@
 fn <- "household_power_consumption.txt"
-data <- read.table(fn,header=TRUE,sep=";")
+data <- read.table(fn,header=TRUE,sep=";",as.is=TRUE, na.strings="?")
 filtered <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007",]
 dateTime <- paste(filtered$Date, filtered$Time)
 dateTime <- strptime(dateTime,"%d/%m/%Y %H:%M:%S")
 filtered <- cbind(dateTime, filtered[,-c(1:2)])
+rm(data)
 
 png(filename="plot2.png",  width = 480, height = 480)
-plot(filtered$dateTime, 
-     as.numeric(as.character(filtered$Global_active_power)), 
-     type="l", 
-     ylab="Global Active Power (kilowatts)", 
-     xlab="")
+with(filtered,
+     plot(dateTime, 
+          as.numeric(Global_active_power), 
+          type="l", 
+          ylab="Global Active Power (kilowatts)", 
+          xlab="")
+)
 dev.off()
