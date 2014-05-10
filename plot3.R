3b8a95d29d6df953a82ba3391e9266af7543e9f6
Chris Scott <cscott83@gmail.com>
2014-05-10 13:58:16 -0400

    Initial commit of plot3.R and plot3.png

---
+++ plot3.R
@@ -0,0 +1,29 @@
+fn <- "household_power_consumption.txt"
+data <- read.table(fn,header=TRUE,sep=";",as.is=TRUE, na.strings="?")
+filtered <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007",]
+dateTime <- paste(filtered$Date, filtered$Time)
+dateTime <- strptime(dateTime,"%d/%m/%Y %H:%M:%S")
+filtered <- cbind(dateTime, filtered[,-c(1:2)])
+rm(data)
+
+png(filename="plot3.png",  width = 480, height = 480)
+with(filtered, {
+     plot(dateTime, as.numeric(Sub_metering_1), 
+          type="l",
+          col="black",
+          ylab="Energy sub metering", 
+          xlab="")
+
+     lines(dateTime, as.numeric(Sub_metering_2),
+           type="l",
+           col="red")
+
+     lines(dateTime, as.numeric(Sub_metering_3),
+           type="l",
+           col="blue")
+})
+legend("topright", 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col=c("black", "red", "blue"), 
+       lty=1)
+dev.off()
